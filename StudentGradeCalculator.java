4080a9e7773b7311ed18d855132fd5faa00231e2
padmanitgithub <160648961+padmanitgithub@users.noreply.github.com>
2024-02-21 22:28:59 +0530

    StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -5,14 +5,12 @@ public class StudentGradeCalculator{
 	public static void main(String[] args) {
 		float score=0;
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Student name:");
+		String name=sc.nextLine();
 		try(sc) {
-			System.out.println("Enter the number of students:");
-			int stu=sc.nextInt();
-			for(int s=1;s<=stu;s++) {
-				System.out.println("Enter the Student "+s+" marks");
 			int marks[]=new int[8];//Assuming 7 Subjects
 			for(int i=1;i<marks.length;i++) {
-				System.out.println("Subject"+i+":");
+				System.out.println("Subject "+i+" Marks:");
 				marks[i]=sc.nextInt();
 				if(marks[i]>100) {
 					System.out.println("marks must be between 1-100");
@@ -22,21 +20,25 @@ public class StudentGradeCalculator{
 			}
 			}
 			if(score>0.0) {
+				System.out.println("Student Name:"+name);
 				System.out.println("Student total marks:"+score);
 			System.out.println("Student Score:"+score/7);
 			score=score/7;
 			if(score>=90.0) {
-				System.out.println("Student grade is:"+"A");	
+				System.out.println("Student grade is:"+"S");	
 			}else if(score>=80.0 ) {
-				System.out.println("Student grade is:"+"B");
+				System.out.println("Student grade is:"+"A");
 			}else if(score>=70.0) {
-				System.out.println("Student grade is:"+"C");
+				System.out.println("Student grade is:"+"B");
 			}else if(score>=60.0){
 				System.out.println("Student grade is:"+"C");
+			}else if(score>=50) {
+				System.out.println("Student grade is :D");
+			}else {
+				System.out.println("Student grade is :Fail");
 			}
 			}
 			}
-	}
 		catch(InputMismatchException e) {
 			System.out.println("Marks should be in numbers");
 		}
